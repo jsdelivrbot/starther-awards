3ee7142b6a29883d62024e97673355de6823fb31
Nazim Saouli <nazim.saouli@datadoghq.com>
2017-10-16 14:40:44 +0200

    Body Parser

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const Sequelize = require('sequelize');
@@ -16,6 +16,7 @@ Like.sync({ force: true });
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
