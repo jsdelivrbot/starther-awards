9b81d7606350bf6d328f8d9cd748612c9abdc9a4
Nadir Kadem <nadir.kadem@datadoghq.com>
2017-10-16 11:32:26 +0200

    bla

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 const Sequelize = require('sequelize');
 const { likeDefinition } = require('./db-models');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-const Like = sequelize.define('like', likeDefinition);
-Like.sync();
+// const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const Like = sequelize.define('like', likeDefinition);
+// Like.sync();
 
 app.set('port', process.env.PORT || 5000);
 
@@ -18,30 +18,34 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-    response.render('pages/index');
+// app.get('/', function(request, response) {
+//     response.render('pages/index');
+// });
+
+// app.get('/likes', (req, res) => {
+//     Like.findAll().then(likes => res.send(likes));
+// });
+
+// app.post('/likes', (req, res) => {});
+
+// app.delete('/likes', (req, res) => {});
+
+app.listen(app.get('port'), function() {
+    console.log(chalk.cyan('Node app is running on port', app.get('port')));
 });
 
-app.get('/likes', (req, res) => {
-    Like.findAll().then(likes => res.send(likes));
-});
-
-app.post('/likes', (req, res) => {});
-
-app.delete('/likes', (req, res) => {});
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log(
-            chalk.green('DB Connection has been established successfully.')
-        );
-        app.listen(app.get('port'), function() {
-            console.log(
-                chalk.cyan('Node app is running on port', app.get('port'))
-            );
-        });
-    })
-    .catch(err => {
-        console.error(chalk.red('Unable to connect to the database:', err));
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log(
+//             chalk.green('DB Connection has been established successfully.')
+//         );
+//         app.listen(app.get('port'), function() {
+//             console.log(
+//                 chalk.cyan('Node app is running on port', app.get('port'))
+//             );
+//         });
+//     })
+//     .catch(err => {
+//         console.error(chalk.red('Unable to connect to the database:', err));
+//     });
